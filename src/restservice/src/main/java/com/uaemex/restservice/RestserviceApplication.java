476ba253d041bb5cbf1b2986ebbc67c1d3e85327
Ricardo Perez <richartl@Ricardos-MacBook-Pro.local>
2021-12-13 19:43:20 -0600

    Fix database on srping project

--- src/restservice/src/main/java/com/uaemex/restservice/RestserviceApplication.java
+++ src/restservice/src/main/java/com/uaemex/restservice/RestserviceApplication.java
@@ -2,8 +2,10 @@ package com.uaemex.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.uaemex.restservice.repositories","com.uaemex.restservice.controllers", "com.uaemex.restservice.dao"})
 public class RestserviceApplication {
 
 	public static void main(String[] args) {
--- src/restservice/src/main/java/com/uaemex/restservice/ServletInitializer.java
+++ src/restservice/src/main/java/com/uaemex/restservice/ServletInitializer.java
@@ -2,7 +2,9 @@ package com.uaemex.restservice;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.uaemex.restservice.repositories","com.uaemex.restservice.controllers", "com.uaemex.restservice.dao"})
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
